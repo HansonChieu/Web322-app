9b21e3f9c7252413ba4e524a2874ecd171ef4850
Hanson Chieu <hansonchieu@hotmail.com>
2024-10-08 20:22:05 -0400

    Updated files

--- server.js
+++ server.js
@@ -1,59 +1,65 @@
-const express = require('express'); // "require" the Express module
+const express = require("express"); // "require" the Express module
 const app = express(); // obtain the "app" object
-const path = require('path');
-const fs= require('fs');
-const storeService = require('./store-service.js');
+const path = require("path");
+const fs = require("fs");
+const storeService = require("./store-service.js");
 
 app.use(express.static("public"));
 
-app.get('/', (req,res)=>{
-  res.redirect('/about');       // Redirect root to the /about route
+app.get("/", (req, res) => {
+  res.redirect("/about"); // Redirect root to the /about route
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/about.html'));    // Send about.html file
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/about.html")); // Send about.html file
 });
 
-app.get('/shop', (req,res)=>{
- storeService.getAllItems()
-  .then((data)=>{
-    const publishedItems = data.filter(item => item.published);
-    res.json(publishedItems);
-  })
-  .catch((err)=>{
-    res.status(500).json({message: err}); 
-  });
+app.get("/shop", (req, res) => {
+  storeService
+    .getAllItems()
+    .then((data) => {
+      const publishedItems = data.filter((item) => item.published);
+      res.json(publishedItems);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
-app.get('/items', (req,res)=>{
-  storeService.getAllItems()
-   .then((data)=>{
-    res.json(data);
-   })
-   .catch((err)=>{
-     res.status(500).json({message: err}); 
-   });
- });
+app.get("/items", (req, res) => {
+  storeService
+    .getAllItems()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
+});
 
- app.get('/categories', (req,res)=>{
-  storeService.getCategories()
-   .then((data)=>{
-     res.json(data);
-   })
-   .catch((err)=>{
-     res.status(500).json({message: err}); 
-   });
- });
+app.get("/categories", (req, res) => {
+  storeService
+    .getCategories()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
+});
 
 app.use((req, res) => {
-  res.status(404).send("Page Not Found"); 
+  res.status(404).send("Page Not Found");
 });
 
-storeService.initialize()
-.then(()=>{
-  const HTTP_PORT = process.env.PORT || 8080; // assign a port
-  app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
-})
-.catch((error) => {
-  console.error("Failed to initialize data:", error);
-});
+storeService
+  .initialize()
+  .then(() => {
+    const HTTP_PORT = process.env.PORT || 8080; // assign a port
+    app.listen(HTTP_PORT, () =>
+      console.log(`server listening on: ${HTTP_PORT}`),
+    );
+  })
+  .catch((error) => {
+    console.error("Failed to initialize data:", error);
+  });
